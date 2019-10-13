d68fb0323ab27b0283767b06a9580838d6ce4707
Simon Symhoven <simon.symhoven@hm.edu>
2019-10-13 12:21:39 +0200

    inittial commit

---
+++ DakoChatApplication/audit_log_server/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogUdpServer.java
@@ -0,0 +1,29 @@
+package edu.hm.dako.chat.AuditLogServer;
+
+import edu.hm.dako.chat.common.AuditLogPDU;
+import org.apache.log4j.Logger;
+
+public class AuditLogUdpServer {
+
+	private static Logger log = Logger.getLogger(AuditLogUdpServer.class);
+
+	// UDP-Serverport fuer AuditLog-Service
+	static final int AUDIT_LOG_SERVER_PORT = 40001;
+
+	// Standard-Puffergroessen fuer Serverport in Bytes
+	static final int DEFAULT_SENDBUFFER_SIZE = 30000;
+	static final int DEFAULT_RECEIVEBUFFER_SIZE = 800000;
+
+	// Name der AuditLog-Datei
+	static final String auditLogFile = new String("ChatAuditLog.dat");
+
+	// Zaehler fuer ankommende AuditLog-PDUs
+	protected long counter = 0;
+
+	public static void main(String[] args) {
+		System.out.println("AuditLog-UdpServer gestartet, Port: " + AUDIT_LOG_SERVER_PORT);
+		log.info("AuditLog-UdpServer gestartet, Port" + AUDIT_LOG_SERVER_PORT);
+
+		//TODO: AuditLog-Server implementieren
+	}
+}
