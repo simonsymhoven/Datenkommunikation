4a58511a08d3c75659bdec0d4c414357bcf1283e
OttNorml <OttNorml@gmail.com>
2019-12-12 07:01:45 +0100

    [#25] Add logging to csv file

--- DakoChatApplication/src/main/java/edu/hm/dako/chat/audit_log_server/MessageLoggingWorkerThread.java
+++ DakoChatApplication/src/main/java/edu/hm/dako/chat/audit_log_server/MessageLoggingWorkerThread.java
@@ -4,12 +4,21 @@ import edu.hm.dako.chat.common.AuditLogPDU;
 import edu.hm.dako.chat.common.ExceptionHandler;
 import org.apache.log4j.Logger;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.List;
 
 public class MessageLoggingWorkerThread extends Thread {
     private static final Logger log = Logger.getLogger(MessageLoggingWorkerThread.class);
     private final AuditLogPduDaoInterface<AuditLogPDU> model;
     private int messageCounter = 0;
+    private BufferedWriter csvWriter;
 
     public MessageLoggingWorkerThread(AuditLogPduDaoInterface<AuditLogPDU> model) {
         this.model = model;
@@ -18,7 +27,7 @@ public class MessageLoggingWorkerThread extends Thread {
 
     /**
      * Waits for new {@link AuditLogPDU} messages from the {@link AuditLogPduDaoInterface#getAllNew()}
-     * and then processes them.
+     * and then logs them to a csv log file.
      */
     @Override
     public void run() {
@@ -27,15 +36,75 @@ public class MessageLoggingWorkerThread extends Thread {
                 final List<AuditLogPDU> allNew = model.getAllNew();
                 messageCounter += allNew.size();
                 log.debug("Anzahl neuer Nachrichten: " + messageCounter);
-                // TODO: Add logging
                 for (AuditLogPDU auditLogPDU : allNew) {
                     log.debug(auditLogPDU);
+                    getCsvWriter()
+                        .append(auditLogPDU.getPduType().toString())
+                        .append(';')
+                        .append(auditLogPDU.getClientThreadName())
+                        .append(';')
+                        .append(auditLogPDU.getServerThreadName())
+                        .append(';')
+                        .append(auditLogPDU.getUserName())
+                        .append(';')
+                        .append(new Date(auditLogPDU.getAuditTime()).toString())
+                        .append(';');
+                    getCsvWriter()
+                        .newLine();
                 }
+                getCsvWriter().flush();
                 Thread.sleep(1000);
-            } catch (InterruptedException e) {
+            } catch (InterruptedException | IOException e) {
                 ExceptionHandler.logException(e);
                 Thread.currentThread().interrupt();
             }
         }
+    }
+
+    /**
+     * If {@link MessageLoggingWorkerThread#csvWriter} is @null, a new {@link BufferedWriter} object is created and
+     * returned. As a result a new CSV file will be created as well.
+     *
+     * @return new or existing {@link MessageLoggingWorkerThread#csvWriter}
+     * @throws IOException If an I/O error occurs
+     */
+    private BufferedWriter getCsvWriter() throws IOException {
+        if (csvWriter == null) {
+            final File logDir = new File(System.getProperty("user.dir") + File.separator + "logs");
+            if (!logDir.exists()) {
+                //noinspection ResultOfMethodCallIgnored
+                logDir.mkdirs();
+            }
+
+            //noinspection SpellCheckingInspection
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmssZ").withZone(ZoneId.of("UTC"));
+            String logFileDate = formatter.format(Instant.now());
+
+            File logFile = new File(logDir.getAbsolutePath() + File.separator + "audit_log_" + logFileDate + ".csv");
+
+            csvWriter = new BufferedWriter(new FileWriter(logFile, true));
+
+            createLogHeaderLine();
+        }
+
+        return csvWriter;
+    }
+
+    /**
+     * @throws IOException If an I/O error occurs
+     */
+    private void createLogHeaderLine() throws IOException {
+        csvWriter
+            .append("PduType")
+            .append(';')
+            .append("ClientThreadName")
+            .append(';')
+            .append("ServerThreadName")
+            .append(';')
+            .append("UserName")
+            .append(';')
+            .append("AuditTime")
+            .append(';');
+        csvWriter.newLine();
     }
 }
