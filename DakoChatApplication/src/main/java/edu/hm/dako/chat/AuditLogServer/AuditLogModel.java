8239369e073d091a591508f15d512d9cfd2a7a41
OttNorml <OttNorml@gmail.com>
2019-12-12 07:08:44 +0100

    [#25] Remove old AuditLogServer implementation and revert ChatServerGUI to 6d09bec

--- DakoChatApplication/src/main/java/edu/hm/dako/chat/AuditLogServer/AuditLogModel.java
+++
@@ -1,9 +0,0 @@
-package edu.hm.dako.chat.AuditLogServer;
-
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-
-public class AuditLogModel {
-    public ObservableList<String> messages = FXCollections.observableArrayList();
-    public String serverAddress;
-}
